bad1cdf554a3fcf2ba7f2157768562cff38b7436
Jan Jaap de Groot <info@janjaap.com>
2026-01-05 11:13:17 +0100

    Improve compatibility

--- Package.swift
+++ Package.swift
@@ -1,28 +1,42 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "AbraAPI",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15)
+        .iOS(.v17),
+        .macOS(.v14)
     ],
     products: [
+        // Library with dependencies
         .library(
             name: "AbraAPI",
-            targets: ["AbraAPIFramework"]
+            targets: ["AbraAPIFramework", "AbraAPIWrapper"]
         ),
     ],
     dependencies: [
-        // All dependencies are embedded in the binary
+        // Dependencies that the binary framework needs
+        .package(url: "https://github.com/mac-cain13/R.swift.git", .upToNextMajor(from: "7.7.0")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.1"))
     ],
     targets: [
+        // Binary target containing the framework
         .binaryTarget(
             name: "AbraAPIFramework",
             url: "https://nbg1.your-objectstorage.com/abrabv/AbraAPI/1.1.1/AbraAPI-1.1.1.zip",
             checksum: "060ecc16c066a8f97abf8624c5d06ebad052d2f837c22f1b54472323e7fdd29a"
         ),
+        // Wrapper target to handle binary target dependencies
+        .target(
+            name: "AbraAPIWrapper",
+            dependencies: [
+                .product(name: "RswiftLibrary", package: "R.swift"),
+                .product(name: "Alamofire", package: "Alamofire"),
+                "AbraAPIFramework"
+            ],
+            path: "Wrapper"
+        )
     ],
     swiftLanguageVersions: [.v5]
 )
