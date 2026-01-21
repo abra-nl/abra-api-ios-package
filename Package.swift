50e7c28787be89999f512d860d5a95bf349cd623
Jan Jaap de Groot <info@janjaap.com>
2026-01-21 11:38:42 +0100

    Re-release 1.1.1

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         // Binary target containing the framework
         .binaryTarget(
             name: "AbraAPIFramework",
-            path: "releases/1.1.0/AbraAPI-1.1.0.zip"
+            path: "releases/1.1.1/AbraAPI-1.1.1.zip"
         ),
         // Wrapper target to handle binary target dependencies
         .target(
